a3066ac449409349b656d6b695399d8adabe064a
luckytyphlosion <10688458+luckytyphlosion@users.noreply.github.com>
2022-11-17 17:19:22 -0500

    Add automatic patching.

--- compile.sh
+++ compile.sh
@@ -15,3 +15,10 @@ head -c -1 exe6f_us-unseniors-temp.sym | cat - constants_ewram.sym > exe6f_us-un
 head -c -1 exe6g_us-unseniors-temp.sym | cat - constants_ewram.sym > exe6g_us-unseniors.sym
 
 rm exe6f-unseniors-temp.sym exe6g-unseniors-temp.sym exe6f_us-unseniors-temp.sym exe6g_us-unseniors-temp.sym
+
+mkdir out/patch_jp out/patch_us
+
+./tools/floating/flips.exe -c -b "exe6f.gba" "exe6f-unseniors.gba" "out/patch_jp/BR6J_00.bps"
+./tools/floating/flips.exe -c -b "exe6g.gba" "exe6g-unseniors.gba" "out/patch_jp/BR5J_00.bps"
+./tools/floating/flips.exe -c -b "exe6f.gba" "exe6f_us-unseniors.gba" "out/patch_us/BR6J_00.bps"
+./tools/floating/flips.exe -c -b "exe6g.gba" "exe6g_us-unseniors.gba" "out/patch_us/BR5J_00.bps"
